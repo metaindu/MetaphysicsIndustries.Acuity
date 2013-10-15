f9d1d21cc21a1fc02a6956292cc5af23dd8a29a2
izrik <izrik@izrik.com>
2013-10-15 08:25:00 -0500

    Replace vars dictionaries with Environment.

--- VariableRotateCoordinatesMatrixFilter.cs
+++ VariableRotateCoordinatesMatrixFilter.cs
@@ -2,30 +2,31 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using MetaphysicsIndustries.Solus;
+using Environment = MetaphysicsIndustries.Solus.Environment;
 
 namespace MetaphysicsIndustries.Acuity
 {
     [Serializable]
     public class VariableRotateCoordinatesMatrixFilter : RotateCoordinatesMatrixFilter
     {
-        public VariableRotateCoordinatesMatrixFilter(Dictionary<string, Expression> varTable, string variable)
+        public VariableRotateCoordinatesMatrixFilter(Environment env, string variable)
             : base(0)
         {
-            if (varTable == null) { throw new ArgumentNullException("varTable"); }
+            if (env == null) { throw new ArgumentNullException("env"); }
             if (variable == null) { throw new ArgumentNullException("variable"); }
 
-            _varTable = varTable;
+            _env = env;
             _variable = variable;
         }
 
-        private Dictionary<string, Expression> _varTable;
+        private Environment _env;
         private string _variable;
 
         public override float Angle
         {
             get
             {
-                return (float)_varTable[_variable].Eval(_varTable).Value;
+                return (float)_env.Variables[_variable].Eval(_env).Value;
             }
         }
     }
